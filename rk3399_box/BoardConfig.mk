d01e4a1a706124beb8faaa7659eac3a6d4622cfb
Hans Yang <yhx@rock-chips.com>
2016-06-14 11:02:43 +0800

    rk3399_box: modify TARGET_CPU_VARIANT and TARGET_2ND_CPU_VARIANT
    Change-Id: I4d12509837257b8264fc407101ff82ffce226a0d
    Signed-off-by: Hans Yang <yhx@rock-chips.com>

--- rk3399_box/BoardConfig.mk
+++ rk3399_box/BoardConfig.mk
@@ -1,17 +1,17 @@
 include device/rockchip/rk3399/BoardConfig.mk
 
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := cortex-a53
 TARGET_CPU_SMP := true
-TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_VARIANT := cortex-a15
 
-TARGET_2ND_ARCH :=
-TARGET_2ND_ARCH_VARIANT :=
-TARGET_2ND_CPU_ABI :=
-TARGET_2ND_CPU_ABI2 :=
-TARGET_2ND_CPU_VARIANT :=
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a15
 
 # Re-enable emulator for 32-bit
 BUILD_EMULATOR := false
