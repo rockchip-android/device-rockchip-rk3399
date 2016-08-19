e05c2ad678c78c2ed19627bf0c7a6acc6145a6f1
wenping.zhang <wenping.zhang@rock-chips.com>
2016-08-19 09:27:50 +0800

    add macro BOARD_USE_DUAL_MIPI to define whether our VR device use dual mipi screen.
    please refer to hardware/rockchip/hwcomposer to find what's modified.
    
    Change-Id: I4ff736c551fc9717773bc47bbf52a3f2763e6305

--- rk3399_64_vr/BoardConfig.mk
+++ rk3399_64_vr/BoardConfig.mk
@@ -5,4 +5,7 @@ BOARD_SENSOR_ST := false
 BOARD_SENSOR_MPU := false
 BOARD_SENSOR_MPU_VR := true
 
+# set true if you use dual mipi screen.
+BOARD_USE_DUAL_MIPI := false
+
 TARGET_BOARD_PLATFORM_PRODUCT := vr
