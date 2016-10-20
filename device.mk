fc704839a5a8de7550d1967a4e36432b557789eb
HaoXiaowei <hxw@rock-chips.com>
2016-10-20 15:55:26 +0800

    rk3399:Change some permission for gpu & ddr related node
    1.Add init.rk3399.rc to add some common platform operations.
    2.Change these gpu & ddr permission for some power hal.
    
    Change-Id: Id0d5e115984e5e8f3b7c7a38345aff8f84fa4580
    Signed-off-by: HaoXiaowei <hxw@rock-chips.com>

--- device.mk
+++ device.mk
@@ -41,6 +41,7 @@ endif
 #endif
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/init.${TARGET_BOARD_PLATFORM}.rc:root/init.${TARGET_BOARD_PLATFORM}.rc \
     $(LOCAL_PATH)/init.${TARGET_BOARD_PLATFORM_PRODUCT}.rc:root/init.${TARGET_BOARD_PLATFORM_PRODUCT}.rc \
     $(LOCAL_PATH)/debug/init.debug.rc:root/init.debug.rc \
 	$(LOCAL_PATH)/init.rk30board.usb.rc:root/init.rk30board.usb.rc
