044d345526895bee9cab0a0b2d416e5bff979174
Andy Ye <Andy.Ye@rock-chips.com>
2016-11-26 11:59:24 +0800

    BoardConfig.mk:3399:nougat:add marco for sepolicy
    Change-Id: I157a9c7bb14466dc3d2b566efe341e0cfa7a78c4

--- rk3399.mk
+++ rk3399.mk
@@ -43,3 +43,5 @@ PRODUCT_MANUFACTURER := rockchip
 # Get the long list of APNs
 PRODUCT_COPY_FILES += vendor/rockchip/common/phone/etc/apns-full-conf.xml:system/etc/apns-conf.xml
 PRODUCT_COPY_FILES += vendor/rockchip/common/phone/etc/spn-conf.xml:system/etc/spn-conf.xml
+
+TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS ?= true
