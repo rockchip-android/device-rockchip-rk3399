ac17d7bc82243f9da90455d1b8b49e41ecd3658c
wlq <wlq@rock-chips.com>
2017-06-20 10:27:14 +0800

    rk3399-mid: enabled st sensor
    Change-Id: I934738088c7a961bd6130615b182a21228ffc3c9

--- rk3399_mid/BoardConfig.mk
+++ rk3399_mid/BoardConfig.mk
@@ -1,9 +1,9 @@
 include device/rockchip/rk3399/BoardConfig.mk
 
 # Sensors
-BOARD_SENSOR_ST := false
+BOARD_SENSOR_ST := true
 BOARD_SENSOR_MPU := false
-BOARD_SENSOR_MPU_VR := true
+BOARD_SENSOR_MPU_VR := false
 
 BOARD_SENSOR_COMPASS_AK8963-64 := true
 
