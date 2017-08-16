3c00204ab885d5775078d9d459198690c0ec77b8
Huacong Yang <will.yang@rock-chips.com>
2017-08-16 20:30:06 +0800

    rk3399 box: enable mtp default
    Change-Id: Ibb18d0c5fc1750d1b8647ac9d00a10bd9a1365f6

--- rk3399_box.mk
+++ rk3399_box.mk
@@ -28,6 +28,9 @@ PRODUCT_MODEL := rk3399-box
 PRODUCT_AAPT_CONFIG := normal tvdpi hdpi
 PRODUCT_AAPT_PREF_CONFIG := tvdpi
 
+# enable mtp default
+BOARD_USB_ALLOW_DEFAULT_MTP := true
+
 # debug-logs
 ifneq ($(TARGET_BUILD_VARIANT),user)
 MIXIN_DEBUG_LOGS := true
