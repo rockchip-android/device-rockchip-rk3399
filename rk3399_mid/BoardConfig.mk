cbcbe6d42cd604a5cda9fefaee4be98d7df9d3c0
Zorro Liu <lyx@rock-chips.com>
2017-12-12 18:04:21 +0800

    rk3399 mid: sensor default use mpu_pad for excavator board
    Change-Id: I370319b55662ddf8a50944fc2bdfa276d15f2a37
    Signed-off-by: Zorro Liu <lyx@rock-chips.com>

--- rk3399_mid/BoardConfig.mk
+++ rk3399_mid/BoardConfig.mk
@@ -1,8 +1,8 @@
 include device/rockchip/rk3399/BoardConfig.mk
 
 # Sensors
-BOARD_SENSOR_ST := true
-BOARD_SENSOR_MPU := false
+BOARD_SENSOR_ST := false
+BOARD_SENSOR_MPU_PAD := true
 BOARD_SENSOR_MPU_VR := false
 
 BOARD_SENSOR_COMPASS_AK8963-64 := true
